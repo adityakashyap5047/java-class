630a42d46f85a384e7a4aace6a8fc2297e0a145a
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-01-17 14:54:53 +0530

    Uploading initial files into github

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers{
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("The sum of 5 and 10 is = "+ a+b);
+    }
+}
