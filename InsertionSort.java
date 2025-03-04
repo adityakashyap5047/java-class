c97f1be4e3d16c1586c7b0203ea2b6f920444e83
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-04 20:14:46 +0530

    insertion sort in java

---
+++ InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+
+    public static void insertionSort(int numbers[]){
+
+        int n = numbers.length;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int key = numbers[i];
+            int j = i - 1;
+
+            while (j >= 0 && numbers[j] > key) {
+                numbers[j+1] = numbers[j];
+                j = j - 1;
+            }
+            numbers[j + 1] = key;
+        }
+    }
+    
+    public static void main(String[] args) {
+        int numbers[] = {5, 1, 4, 9, 2, 7};
+
+        insertionSort(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
