3c0cd561412064531462c24f03264d4f852821fc
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-21 00:48:43 +0530

    completing the message sent with synchronization

--- SynchronizedMessage.java
+++ SynchronizedMessage.java
@@ -1,6 +1,3 @@
-import java.io.*;
-import java.util.*;
-
 class Sender{
     public void send(String msg){
         System.out.println("Sending " + msg);
@@ -9,11 +6,41 @@ class Sender{
         } catch(Exception e){
             System.out.println("Thread Interrupted");
         }
-        System.out.println(msg + "Sent");
+        System.out.println(msg + " Sent");
+    }
+}
+
+class ThreadSend extends Thread{
+    private String msg;
+    Sender sender;
+
+    ThreadSend(String m, Sender obj){
+        this.msg = m;
+        this.sender = obj;
+    }
+
+    public void run(){
+        synchronized(sender){
+            sender.send(msg);
+        }
     }
 }
 
 public class SynchronizedMessage {
     
+    public static void main(String[] args) {
+        Sender send = new Sender();
+        ThreadSend s1 = new ThreadSend("HIII", send);
+        ThreadSend s2 = new ThreadSend("Byyy", send);
 
+        s1.start();
+        s2.start();
+
+        try{
+            s1.join();
+            s2.join();
+        } catch (Exception e){
+            System.out.println("Interrupted");
+        }
+    }
 }
