085bcbc511f6321c36ea65e946432585194a8565
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-22 00:28:26 +0530

    creating the function for adding the element at the begging  of the linked list

--- Add1stInLL.java
+++ Add1stInLL.java
@@ -21,12 +21,35 @@ public class Add1stInLL {
         System.out.println("null");
     }
 
+    public static Node add1st(Node head, int data){
+
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            return newNode;
+        }
+        else{
+            newNode.next = head;
+            return newNode;
+        }
+    }
+
     public static void main(String[] args) {
         Node head = new Node(5);
         head.next = new Node(10);
         head.next.next = new Node(15);
 
         printLL(head);
+        printLL(add1st(head, 0));
+
+        Node temp = null;
+        temp = add1st(temp, 10);
+        temp = add1st(temp, 15);
+        temp = add1st(temp, 20);
+        temp = add1st(temp, 30);
+        temp = add1st(temp, 40);
+
+        printLL(temp);
     }
 
 }
