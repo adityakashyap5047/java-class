db5ad99b1c95600275db30042f1bab5e45c7133d
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-23 09:25:09 +0530

    add last in linked list

--- AddLastInLL.java
+++ AddLastInLL.java
@@ -23,12 +23,40 @@ public class AddLastInLL {
         }
         System.out.println("null");
     }
+
+    public static Node addLastInLL(Node head, int data){
+
+        Node newNode = new Node(data);
+        Node temp = head;
+
+        if (temp == null) {
+            return newNode;
+        }
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+
+        return head;
+    }
+
     public static void main(String[] args) {
         Node head = new Node(5);
         head.next = new Node(10);
         head.next.next = new Node(15);
 
         printLL(head);
+        head = addLastInLL(head, 20);
+        printLL(head);
 
+        Node temp = null;
+        printLL(temp);
+        temp = addLastInLL(temp, 2);
+        temp = addLastInLL(temp, 4);
+        temp = addLastInLL(temp, 6);
+        temp = addLastInLL(temp, 8);
+        printLL(temp);
     }
 }
